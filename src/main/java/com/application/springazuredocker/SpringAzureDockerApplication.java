9abbb79355b68e38b9fa775d8dfba90eed0bf4db
John Gualteros <jagualterosg@kumpels.com.co>
2023-07-15 22:08:23 -0500

    🔧 Adding the docker for postgresql

--- src/main/java/com/application/springazuredocker/SpringAzureDockerApplication.java
+++ src/main/java/com/application/springazuredocker/SpringAzureDockerApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringAzureDockerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SpringAzureDockerApplication.class, args);
     }
-
 }
