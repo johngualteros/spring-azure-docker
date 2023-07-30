00dcbf3c0567a338dfdb19b8b1b490206a06c0b1
John Gualteros <jagualterosg@kumpels.com.co>
2023-07-30 14:28:02 -0500

    🏗️ Adding the user repository and entity for Tag

--- src/main/java/com/application/springazuredocker/tag/infrastructure/entity/Tag.java
+++ src/main/java/com/application/springazuredocker/tag/infrastructure/entity/Tag.java
@@ -1,7 +1,6 @@
 package com.application.springazuredocker.tag.infrastructure.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
@@ -10,9 +9,16 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "tag")
 public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long   id;
+    @Column
+    private String uuid;
+    @Column
     private String name;
+    @Column
     private String color;
+    @Column
     private String description;
     // TODO: add here the owner of tag
 }
