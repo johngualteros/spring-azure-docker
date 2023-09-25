0e42d5c50b3e0d48a32e60c3f11e5e788c77385e
john <gualterosjohn40@gmail.com>
2023-09-25 15:40:04 -0500

    ➖ del: Removing docker file

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM ubuntu:latest
-LABEL authors="john"
-WORKDIR /app
-EXPOSE 8081
--- src/main/java/com/application/springazuredocker/tag/infrastructure/controller/TagGrpc.java
+++
@@ -1,5 +0,0 @@
-package com.application.springazuredocker.tag.infrastructure.controller;
-
-//TODO: add grpc controller
-public class TagGrpc {
-}
